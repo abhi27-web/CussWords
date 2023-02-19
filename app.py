8f65d534bb3360af1122812416ea4ce517488f80
abhi27-web <57997268+abhi27-web@users.noreply.github.com>
2023-02-19 21:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from flask import Flask, request
-# from joblib import load
-# from profanity_filter import ProfanityFilter
+from flask import Flask, jsonify, request
+from profanity_filter import ProfanityFilter
 
 app = Flask(__name__)
 
 @app.route('/profanity', methods=['POST'])
-def sort():
+def check_profanity():
     data = request.get_json()
     sentence = data['sentence']
-#     pf = ProfanityFilter()
-#     output = pf.censor(sentence)
-    return sentence
+    pf = ProfanityFilter()
+    output = pf.censor(sentence)
+    return jsonify({'result': output})
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run()
