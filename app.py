a29bc25c203350cdac02fbbdaa42c685e6d848e9
abhi27-web <57997268+abhi27-web@users.noreply.github.com>
2023-02-19 10:52:47 +0530

    Update and rename main.py to app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+import profanity_check
+import emoji
+
+app = Flask(__name__)
+
+@app.route('/check', methods=['POST'])
+def check_profanity():
+    text = request.json['text']
+    censored_text = profanity_check.censor(text)
+    censored_text = emoji.emojize(censored_text, use_aliases=True)
+    return jsonify({'censored_text': censored_text})
+
+if __name__ == '__main__':
+    app.run()
