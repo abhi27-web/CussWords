39104c21d86fe5c534b422e3147c0f5217db2d1f
abhi27-web <57997268+abhi27-web@users.noreply.github.com>
2023-02-19 14:28:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import profanity_check
+from better_profanity import profanity
 
 app = Flask(__name__)
 
@@ -7,20 +7,8 @@ app = Flask(__name__)
 def check_profanity():
     data = request.get_json()
     sentence = data['sentence']
-    words = sentence.split()
-
-    # Check for profanity in the sentence
-    if profanity_check.predict(words):
-        # Replace profanity with symbols
-        cleaned_sentence = ""
-        for word in words:
-            if profanity_check.predict([word]):
-                cleaned_sentence += "*" * len(word) + " "
-            else:
-                cleaned_sentence += word + " "
-        return {"cleaned_sentence": cleaned_sentence.strip()}
-    else:
-        return {"cleaned_sentence": sentence}
+    output = profanity.censor(item.Text)
+    return output
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
